47114ffa7b4f5f9004630d81e57bbf0e7d3e2ad0
2019040782 <recsater@hanyang.ac.kr>
2021-04-04 22:48:23 +0900

    branch main

--- app/src/main/java/com/instructor/manito/MainActivity.kt
+++ app/src/main/java/com/instructor/manito/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState)  
         setContentView(binding.root)
         Log.d("test", "test")
     }
