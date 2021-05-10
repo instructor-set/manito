1927060465c39d6b92182544f9fb444f4b9a9e57
grace915 <55349655+grace915@users.noreply.github.com>
2021-05-10 20:31:55 +0900

    가은 커밋

--- app/src/main/java/com/instructor/manito/LobbyActivity.kt
+++ app/src/main/java/com/instructor/manito/LobbyActivity.kt
@@ -7,5 +7,6 @@ class LobbyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lobby)
+
     }
 }
