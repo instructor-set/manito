576be7e7f0c02078602b1b2014a4af2c85fdb10b
2019040782 <recsater@hanyang.ac.kr>
2021-05-17 23:56:42 +0900

    안ㄴ여하세요

--- app/src/main/java/com/instructor/manito/MainActivity.kt
+++ app/src/main/java/com/instructor/manito/MainActivity.kt
@@ -3,9 +3,12 @@ package com.instructor.manito
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.instructor.manito.databinding.ActivityMainBinding
-import splitties.activities.start
+import splitties.fragments.addToBackStack
+import splitties.fragments.fragmentTransaction
 
 class MainActivity : AppCompatActivity() {
+
+    private val adminRoomFragment = AdminRoomFragment.newInstance(1, "아무나 들어와라", 1234, "진하?")
 
     private val main by lazy {
         ActivityMainBinding.inflate(layoutInflater)
@@ -15,7 +18,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(main.root)
         main.button.setOnClickListener {
-            start<LobbyActivity>()
+            adminRoomFragment.change()
         }
+        main.roomButton.setOnClickListener {
+            fragmentTransaction(false) {
+                replace(R.id.mainFrame, adminRoomFragment)
+                addToBackStack()
+            }
+        }
+
     }
 }
