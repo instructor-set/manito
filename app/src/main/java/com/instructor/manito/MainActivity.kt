9c117e44fbedef0b99aec0d7fcd8a07d41b3be73
2019040782 <recsater@hanyang.ac.kr>
2021-07-28 08:15:23 +0900

    o(TヘTo) 1. MainActivity.kt - 로그인 안 되어있는 경우 종료되도록 하였음 2. RoomActivity.kt - 메뉴 서랍 추가. 현재는 참가자만 표시되어 있음

--- app/src/main/java/com/instructor/manito/MainActivity.kt
+++ app/src/main/java/com/instructor/manito/MainActivity.kt
@@ -8,15 +8,14 @@ import android.util.Log
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.*
 import com.google.firebase.database.ktx.getValue
 import com.instructor.manito.databinding.ActivityMainBinding
 import com.instructor.manito.databinding.AlertdialogEdittextBinding
 import com.instructor.manito.dto.Room
 import com.instructor.manito.lib.Authentication
 import com.instructor.manito.lib.Database
+import com.instructor.manito.lib.Util
 import splitties.activities.start
 import splitties.toast.toast
 
@@ -29,17 +28,17 @@ class MainActivity : AppCompatActivity() {
     private val dataList = arrayListOf<Room>()
     private val adapter = MainRoomAdapter(this@MainActivity, dataList)
 
-    //내가 들어간 방
-    private val roomIEntered = arrayListOf<Room>()
+    // 내가 들어간 방
+    private val myRoomList = arrayListOf<Room>()
+    private val roomAdapter = MainMyRoomAdapter(this@MainActivity, myRoomList)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (!Authentication.isLoggedIn()) {
-            finish()
-        }
         with(bind) {
             setContentView(root)
-
+            if (!Authentication.isLoggedIn()) {
+                finish()
+            }
             // 방 만들기
             createRoomButton.setOnClickListener {
                 start<CreateActivity> {
@@ -62,28 +61,15 @@ class MainActivity : AppCompatActivity() {
                 }
 
             }
-            /*
-            Database.getReference("rooms").addValueEventListener(object : ValueEventListener {
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    dataList.clear()
-                    for (a in snapshot.children) {
-                        dataList.add(a.getValue<Room>()!!)
-                    }
-                    adapter.notifyDataSetChanged()
-                }
-
-                override fun onCancelled(error: DatabaseError) {
-                    Util.j(error.details)
-                }
-
-            })
-
-             */
-
 
             // 어댑터 연결
             mainRecycler.adapter = adapter
             mainRecycler.layoutManager = LinearLayoutManager(this@MainActivity)
+
+            // 내가 들어간 방 어댑터 연결
+            mainManitoRoomRecycler.layoutManager =
+                LinearLayoutManager(this@MainActivity, LinearLayoutManager.HORIZONTAL, false)
+            mainManitoRoomRecycler.adapter = roomAdapter
 
             // 새로고침
             //bind.swipeRefreshLayout.setRefreshStyle(PullRefreshLayout.STYLE_CIRCLES);
@@ -97,24 +83,24 @@ class MainActivity : AppCompatActivity() {
                     swipeRefreshLayout.setRefreshing(false)
                 }
             }
-            val updates: MutableMap<String, Any> = HashMap()
 
         }
 
+
         val mDatabase = Database.getReference("rooms")
         mDatabase.child(Authentication.uid.toString()).addValueEventListener(object :
-            ValueEventListener{
+            ValueEventListener {
             override fun onCancelled(error: DatabaseError) {
                 TODO("Not yet implemented")
             }
 
             override fun onDataChange(snapshot: DataSnapshot) {
-                for(postSnapshot in snapshot.children){
+                for (postSnapshot in snapshot.children) {
                     val name = postSnapshot.child("rooms").getValue(true)
                     Log.e("myName", name.toString())
 
-                }
 
+                }
             }
 
         }
@@ -123,18 +109,13 @@ class MainActivity : AppCompatActivity() {
         )
 
 
-
     }
-
-
-
-
 
 
     //시작할 때 새로고침
     override fun onStart() {
         super.onStart()
-        with (bind) {
+        with(bind) {
             swipeRefreshLayout.setRefreshing(true)
             Database.getReference("rooms").get().addOnSuccessListener {
                 dataList.clear()
@@ -145,45 +126,82 @@ class MainActivity : AppCompatActivity() {
                 swipeRefreshLayout.setRefreshing(false)
 
             }
-            Database.getReference("rooms").get().addOnSuccessListener {
-                for(room in it.children){
-                    roomIEntered.clear()
-                    if(Authentication.uid == room.getValue<Room>()?.manager){
-                        roomIEntered.add(room.getValue<Room>()!!)
+            val roomList = arrayListOf<String>()
+            roomList.clear()
+            myRoomList.clear()
+            Database.getReference("users/${Authentication.uid}/rooms")
+                .addValueEventListener(object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        for (room in snapshot.children) {
+                            roomList.add(room.key.toString())
+                        }
+                        Util.j(roomList.toString())
+                        Database.getReference("rooms")
+                            .addValueEventListener(object : ValueEventListener {
+                                override fun onDataChange(snapshot: DataSnapshot) {
+                                    for (room in snapshot.children) {
+                                        Util.j(room.key.toString())
+                                        if (room.key in roomList) {
+                                            myRoomList.add(room.getValue<Room>()!!)
+                                            Util.j(room.getValue<Room>()!!.toString())
+                                        }
+                                        roomAdapter.notifyDataSetChanged()
+
+                                    }
+                                }
+
+                                override fun onCancelled(error: DatabaseError) {
+                                    TODO("Not yet implemented")
+                                }
+
+                            })
+
+
                     }
 
-                }
-                Log.d("myRoom", roomIEntered.toString())
-            }
+                    override fun onCancelled(error: DatabaseError) {
+                        TODO("Not yet implemented")
+                    }
+
+                })
 
 
         }
 
     }
+    /*
+    패스워드 V
+    방 정보창 V
+    내 방 V
+    비밀번호 사람수 위치 바꾸기 V
+     */
+    /*
+    해야할 거
+    미션 - 1:1 체크
+    랜덤 매칭 알고리즘
+    최대 인원수 저지
+
+     */
+    /*
+    파이어베이스 추가
+    sha 추가
+    figma 얘기해주고
+    지금까지한거 물어보고
+    물어볼거있냐 물어보고
+    최대인원수 추가해줘!
+     */
 
 
+    fun test() {
+        Database.getReference("users/${Authentication.uid}/rooms").get().addOnSuccessListener {
+            for (child in it.children) {
+                Database.getReference("rooms/${child.key}").get().addOnSuccessListener {
+                    val room = it.getValue<Room>()
 
-    //data class Room(var no: Long? = null, var title: String? = null, var password: String? = null, val numberOfPeople: Int? = 0, val participatingUsers: MutableList<User>? = null)
-    //data class User(val nickname: String = "")
-//    fun makeDummyData() : MutableList<Room>{
-//        val data : MutableList<Room> = mutableListOf()
-//
-//        val roomT = Room(0, "test", null)
-//        data.add(roomT)
-//        for(no in 1..10){
-//            val num = no
-//            val title = "${no}번째"
-//            val key = no - 1
-//            val users : MutableList<User> = mutableListOf(User("가은"), User("진하"), User("성덕"))
-//            val room = Room(num.toLong(), title, key.toString(), no * 2, users = users)
-//            Log.e("room", users.toString())
-//            data.add(room)
-//
-//        }
-//
-//        return data
-//    }
+                }
+            }
+        }
+    }
+
 
 }
-
-
