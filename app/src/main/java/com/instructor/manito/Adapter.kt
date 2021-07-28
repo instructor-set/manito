08f53bfd88fb25b2a4d8aae33b0ad4b715f23e88
Duck-E0730 <81903411+Duck-E0730@users.noreply.github.com>
2021-07-28 22:57:11 +0900

    mission 시작

---
+++ app/src/main/java/com/instructor/manito/Adapter.kt
@@ -0,0 +1,10 @@
+package com.instructor.manito
+
+import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
+import com.instructor.manito.dto.Room
+
+class Adapter(val context: Context,val listData: ArrayList<Room>) : RecyclerView.Adapter<Adapter.???>{
+
+
+}
